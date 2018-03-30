ac442ce4a87fe6d484c2441e24e7936c1699d1f3
harveyspectar <ultimatepravin72@gmail.com>
2018-03-30 16:38:58 +0000

    Revert "Enable dexpreopt by default"
    This reverts commit 0abd418bcc86623799ef1c9b5d82ace8d5ff58d9.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,11 +127,9 @@ TARGET_HW_DISK_ENCRYPTION := true
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_DEBUG_INFO := false
-      USE_DEX2OAT_DEBUG := false
-      DONT_DEXPREOPT_PREBUILTS := true
-      WITH_DEXPREOPT_PIC := true
+    endif
   endif
 endif
 WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
